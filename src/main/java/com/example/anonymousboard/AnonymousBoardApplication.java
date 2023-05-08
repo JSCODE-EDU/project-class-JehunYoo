8ca9ea7841f5bffd91d7cd32c5d0a07cc9aca334
JehunYoo <percyu98@gmail.com>
2023-05-08 22:37:20 +0900

    initial commit

---
+++ src/main/java/com/example/anonymousboard/AnonymousBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.anonymousboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonymousBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnonymousBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/anonymousboard/AnonymousBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.anonymousboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonymousBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
