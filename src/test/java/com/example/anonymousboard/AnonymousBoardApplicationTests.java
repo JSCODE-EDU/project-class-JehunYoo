48aafd1410c4ddcd9974c673be61afa31970a309
JehunYoo <percyu98@gmail.com>
2023-05-11 09:23:29 +0900

    test: 게시글 생성 테스트 코드 추가

--- src/test/java/com/example/anonymousboard/AnonymousBoardApplicationTests.java
+++ src/test/java/com/example/anonymousboard/AnonymousBoardApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.anonymousboard;
 
+import com.example.anonymousboard.post.Post;
+import com.example.anonymousboard.post.PostRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AnonymousBoardApplicationTests {
 
+    @Autowired
+    private PostRepository postRepository;
+
+    @Test
+    void testJpa() {
+        for(int i=0; i<3; i++) {
+            Post p = new Post();
+            p.setTitle(String.format("This is title %d", i));
+            p.setContent(String.format("This is content %d", i));
+            this.postRepository.save(p);
+        }
+    }
+
     @Test
     void contextLoads() {
+
     }
 
 }
