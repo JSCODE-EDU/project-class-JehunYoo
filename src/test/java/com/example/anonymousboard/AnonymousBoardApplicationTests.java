0ed5cc93850c50de16446d04c01f34979f5fec48
JehunYoo <percyu98@gmail.com>
2023-05-14 22:21:26 +0900

    test: 테스트 코드 생성일자 추가

--- src/test/java/com/example/anonymousboard/AnonymousBoardApplicationTests.java
+++ src/test/java/com/example/anonymousboard/AnonymousBoardApplicationTests.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
+
 @SpringBootTest
 class AnonymousBoardApplicationTests {
 
@@ -14,10 +16,11 @@ class AnonymousBoardApplicationTests {
 
     @Test
     void testJpa() {
-        for(int i=0; i<3; i++) {
+        for(int i=0; i<200; i++) {
             Post p = new Post();
             p.setTitle(String.format("This is title %d", i));
             p.setContent(String.format("This is content %d", i));
+            p.setCreatedAt(LocalDateTime.now());
             this.postRepository.save(p);
         }
     }
